1f6a437700b01c988d0da9091cc2c120808d60d7
山东小木 <909854136@qq.com>
2022-02-24 15:08:22 +0800

    systemLog优化

--- src/main/java/cn/jbolt/_admin/systemlog/SystemLogService.java
+++ src/main/java/cn/jbolt/_admin/systemlog/SystemLogService.java
@@ -1,6 +1,9 @@
 package cn.jbolt._admin.systemlog;
 
+import cn.jbolt.base.JBoltProSystemLogTargetType;
+import cn.jbolt.core.enumutil.JBoltEnum;
 import cn.jbolt.core.service.JBoltSystemLogService;
+import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
 
 public class SystemLogService extends JBoltSystemLogService {
 
@@ -14,5 +17,54 @@ public class SystemLogService extends JBoltSystemLogService {
 		return null;
 	}
 	
-
+	/**
+	 * 自行扩展log关联URL
+	 */
+	@Override
+	protected String processSystemLogUrl(int targetType, Object targetId) {
+		String url = processJBoltProSystemLogUrl(targetType, targetId);
+		if(notOk(url)) {
+			url = processProjectSystemLogUrl(targetType, targetId);
+		}
+		return url;
+	}
+	/**
+	 * 项目二开扩展
+	 * @param targetType
+	 * @param targetId
+	 * @return
+	 */
+	private String processProjectSystemLogUrl(int targetType, Object targetId) {
+		String url = null;
+		ProjectSystemLogTargetType targetTypeEnum = JBoltEnum.getEnumObjectByValue(ProjectSystemLogTargetType.class, targetType);
+		if(targetTypeEnum == null)
+		switch (targetTypeEnum) {
+//		case HIPRINT_TPL:
+//			url = "admin/hiprinttpl/show/" + targetId;
+//			break;
+		default:
+			break;
+		}
+		return url;
+	}
+	
+	/**
+	 * pro内置处理
+	 * @param targetType
+	 * @param targetId
+	 * @return
+	 */
+	private String processJBoltProSystemLogUrl(int targetType, Object targetId) {
+		String url = null;
+		JBoltProSystemLogTargetType targetTypeEnum = JBoltEnum.getEnumObjectByValue(JBoltProSystemLogTargetType.class, targetType);
+		if(targetTypeEnum == null)
+		switch (targetTypeEnum) {
+		case HIPRINT_TPL:
+			url = "admin/hiprinttpl/show/" + targetId;
+			break;
+		default:
+			break;
+		}
+		return url;
+	}
 }
