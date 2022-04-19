24efb0af8e4688eff2e1b91c83bb88a63b25c015
Kephon <kaifanlai@qq.com>
2022-04-19 13:59:43 +0800

    【调整】跳过测试配置、maven插件及版本配置、jfinal.sh日志输出配置、profile支持、资源文件路径配置、打包配置

--- jfinal.sh
+++ jfinal.sh
@@ -8,7 +8,7 @@
 # 使用说明：
 # 1: 该脚本使用前需要首先修改 MAIN_CLASS 值，使其指向实际的启动类
 #
-# 2：使用命令行 ./jfinal.sh start | stop | restart 可启动/关闭/重启项目  
+# 2：使用命令行 ./jfinal.sh start | stop | restart 可启动/关闭/重启项目
 #
 # 3: JAVA_OPTS 可通过 -D 传入 undertow.port 与 undertow.host 这类参数覆盖
 #    配置文件中的相同值此外还有 undertow.resourcePath、undertow.ioThreads、
@@ -43,13 +43,13 @@ CP=${APP_BASE_PATH}/config:${APP_BASE_PATH}/lib/*
 function start()
 {
     # 运行为后台进程，并在控制台输出信息
-    java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} &
+    # java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} &
 
     # 运行为后台进程，并且不在控制台输出信息
     # nohup java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} >/dev/null 2>&1 &
 
     # 运行为后台进程，并且将信息输出到 output.log 文件
-    # nohup java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} > output.log &
+    nohup java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS} > output.log 2>&1 &
 
     # 运行为非后台进程，多用于开发阶段，快捷键 ctrl + c 可停止服务
     # java -Xverify:none ${JAVA_OPTS} -cp ${CP} ${MAIN_CLASS}
@@ -59,7 +59,7 @@ function stop()
 {
     # 支持集群部署
     kill `pgrep -f ${APP_BASE_PATH}` 2>/dev/null
-    
+
     # kill 命令不使用 -9 参数时，会回调 onStop() 方法，确定不需要此回调建议使用 -9 参数
     # kill `pgrep -f ${MAIN_CLASS}` 2>/dev/null
 
