3e530e10617596f6a137fcb15026cfd753305e89
山东小木 <909854136@qq.com>
2022-09-30 23:36:33 +0800

    词库数据库sql更新上传

--- readme/jbolt_mysql_id_long.sql
+++ readme/jbolt_mysql_id_long.sql
@@ -951,6 +951,6 @@ CREATE TABLE `jb_sensitive_word` (
  `content` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
  `enable` char(1) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1' COMMENT '启用状态',
  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=65250 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='敏感词词库';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='敏感词词库';
 
 SET FOREIGN_KEY_CHECKS = 1;
