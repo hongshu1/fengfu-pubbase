072e11cf8dca6ed122649230d81f80a450b8bea1
山东小木 <909854136@qq.com>
2022-10-06 09:19:11 +0800

    优化openpage的check-handler

--- src/main/webapp/assets/js/jbolt-admin.js
+++ src/main/webapp/assets/js/jbolt-admin.js
@@ -1,4 +1,4 @@
-var jbolt_admin_js_version="6.0.3";
+var jbolt_admin_js_version="6.0.4";
 //拿到window doc和body
 var jboltJsDevMode=false;//当前模式 true是开发调试模式 影响加载插件和jboltlog
 var jboltWindow=$(window);
@@ -12723,13 +12723,15 @@ var OpenPageBtnUtil ={
 						  var exeCheck_handler=eval(checkHandler);
 							if(exeCheck_handler&&typeof(exeCheck_handler)=="function"){
 								checkResult=exeCheck_handler(action);
-								if(!checkResult){
+								if(typeof(checkResult)=="boolean" && checkResult==false){
 									return false;
 								}
-								if(isArray(checkResult)){
-									orignUrl=orignUrl+checkResult.join(",");
-								}else{
-									orignUrl=orignUrl+checkResult;
+								if(typeof(checkResult)!="boolean") {
+									if(isArray(checkResult)) {
+										orignUrl = orignUrl + checkResult.join(",");
+									} else {
+										orignUrl = orignUrl + checkResult;
+									}
 								}
 								 if(this.hasAttribute("href")){
 									 action.attr("href",orignUrl);
