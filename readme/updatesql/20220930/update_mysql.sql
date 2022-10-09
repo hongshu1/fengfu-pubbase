5c92f18e91ba973aaf26e2622886a3fbb6c661e8
山东小木 <909854136@qq.com>
2022-10-09 22:59:37 +0800

    敏感词词库ID主键转雪花策略

--- readme/updatesql/20220930/update_mysql.sql
+++ readme/updatesql/20220930/update_mysql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `jb_sensitive_word` (
-     `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-     `content` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
-     `enable` char(1) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1' COMMENT '启用状态',
-     PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='敏感词词库';
+ `id` bigint(20) NOT NULL COMMENT '主键ID',
+ `content` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
+ `enable` char(1) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1' COMMENT '启用状态',
+ PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '敏感词词库' ROW_FORMAT = Dynamic;
