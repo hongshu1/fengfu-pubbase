43422bb962d209abf2e43e1de931c998b409626f
山东小木 <909854136@qq.com>
2022-10-09 23:08:26 +0800

    sql更新

--- readme/jbolt_mysql_id_long.sql
+++ readme/jbolt_mysql_id_long.sql
@@ -947,10 +947,10 @@ PRIMARY KEY (`id`) USING BTREE
 -- Table structure for jb_sensitive_word
 -- ----------------------------
 CREATE TABLE `jb_sensitive_word` (
- `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+ `id` bigint(20) NOT NULL COMMENT '主键ID',
  `content` varchar(40) COLLATE utf8mb4_general_ci NOT NULL COMMENT '内容',
  `enable` char(1) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '1' COMMENT '启用状态',
  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='敏感词词库';
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '敏感词词库' ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
