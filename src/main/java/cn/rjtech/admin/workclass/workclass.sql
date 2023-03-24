c58c545780c2df7d5f07b993aaa07dc1f3a5fd05
ChenPing <lazp_soft@163.com>
2023-03-24 15:43:54 +0800

    工种档案优化

--- src/main/java/cn/rjtech/admin/workclass/workclass.sql
+++ src/main/java/cn/rjtech/admin/workclass/workclass.sql
@@ -1,22 +1,25 @@
 #sql("list")
-SELECT *
-FROM Bd_WorkClass
-WHERE isDeleted = '0'
-  #if(iautoid)
-  AND iautoid =#para(iautoid)
-  #end
-  #if(cworkclasscode)
-  AND cworkclasscode = #para(cworkclasscode)
-  #end
-  #if(cworkclassname)
-  AND cworkclassname = #para(cworkclassname)
-  #end
-  #if(ilevel)
-  AND ilevel = #para(ilevel)
-  #end
-  #if(isenabled)
-  AND isenabled = #para(isenabled == 'true' ? 1 : 0)
-  #end
-ORDER BY dUpdateTime
-DESC
+SELECT
+    t1.*,
+    Convert(decimal(18,2),t1.isalary) AS isalarydes,
+    t2.name as ileveldes
+FROM Bd_WorkClass t1
+    LEFT JOIN #(getBaseDbName()).dbo.jb_dictionary AS t2 ON t1.ilevel = t2.sn AND  t2.type_key ='work_level'
+WHERE t1.isDeleted = '0'
+#if(iautoid)
+    AND t1.iautoid =#para(iautoid)
+#end
+#if(cworkclasscode)
+    AND t1.cworkclasscode = #para(cworkclasscode)
+#end
+#if(cworkclassname)
+    AND t1.cworkclassname = #para(cworkclassname)
+#end
+#if(ilevel)
+    AND t1.ilevel = #para(ilevel)
+#end
+#if(isenabled)
+    AND t1.isenabled = #para(isenabled == 'true' ? 1 : 0)
+#end
+    ORDER BY t1.dUpdateTime DESC
 #end
