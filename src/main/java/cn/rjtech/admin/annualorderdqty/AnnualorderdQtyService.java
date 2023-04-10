7f797b45f93d9805c3722367a704f92cecdab069
heming <747089552@qq.com>
2023-04-10 18:09:50 +0800

    1.年度计划月度数量明细表表名变动 2.列表查询相关样式更新

---
+++ src/main/java/cn/rjtech/admin/annualorderdqty/AnnualorderdQtyService.java
@@ -0,0 +1,167 @@
+package cn.rjtech.admin.annualorderdqty;
+
+import com.jfinal.plugin.activerecord.Page;
+import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
+import cn.jbolt.core.service.base.BaseService;
+import com.jfinal.kit.Kv;
+import com.jfinal.kit.Ret;
+import cn.jbolt.core.base.JBoltMsg;
+import cn.rjtech.model.momdata.AnnualorderdQty;
+/**
+ * 年度计划订单年月数量 Service
+ * @ClassName: AnnualorderdQtyService
+ * @author: 佛山市瑞杰科技有限公司
+ * @date: 2023-04-10 16:29
+ */
+public class AnnualorderdQtyService extends BaseService<AnnualorderdQty> {
+
+	private final AnnualorderdQty dao = new AnnualorderdQty().dao();
+
+	@Override
+	protected AnnualorderdQty dao() {
+		return dao;
+	}
+
+	/**
+	 * 后台管理分页查询
+	 * @param pageNumber
+	 * @param pageSize
+	 * @param keywords
+	 * @return
+	 */
+	public Page<AnnualorderdQty> paginateAdminDatas(int pageNumber, int pageSize, String keywords) {
+		return paginateByKeywords("iAutoId","DESC", pageNumber, pageSize, keywords, "iAutoId");
+	}
+
+	/**
+	 * 保存
+	 * @param annualorderdQty
+	 * @return
+	 */
+	public Ret save(AnnualorderdQty annualorderdQty) {
+		if(annualorderdQty==null || isOk(annualorderdQty.getIAutoId())) {
+			return fail(JBoltMsg.PARAM_ERROR);
+		}
+		//if(existsName(annualorderdQty.getName())) {return fail(JBoltMsg.DATA_SAME_NAME_EXIST);}
+		boolean success=annualorderdQty.save();
+		if(success) {
+			//添加日志
+			//addSaveSystemLog(annualorderdQty.getIautoid(), JBoltUserKit.getUserId(), annualorderdQty.getName());
+		}
+		return ret(success);
+	}
+
+	/**
+	 * 更新
+	 * @param annualorderdQty
+	 * @return
+	 */
+	public Ret update(AnnualorderdQty annualorderdQty) {
+		if(annualorderdQty==null || notOk(annualorderdQty.getIAutoId())) {
+			return fail(JBoltMsg.PARAM_ERROR);
+		}
+		//更新时需要判断数据存在
+		AnnualorderdQty dbAnnualorderdQty=findById(annualorderdQty.getIAutoId());
+		if(dbAnnualorderdQty==null) {return fail(JBoltMsg.DATA_NOT_EXIST);}
+		//if(existsName(annualorderdQty.getName(), annualorderdQty.getIautoid())) {return fail(JBoltMsg.DATA_SAME_NAME_EXIST);}
+		boolean success=annualorderdQty.update();
+		if(success) {
+			//添加日志
+			//addUpdateSystemLog(annualorderdQty.getIautoid(), JBoltUserKit.getUserId(), annualorderdQty.getName());
+		}
+		return ret(success);
+	}
+
+	/**
+	 * 删除 指定多个ID
+	 * @param ids
+	 * @return
+	 */
+	public Ret deleteByBatchIds(String ids) {
+		return deleteByIds(ids,true);
+	}
+
+	/**
+	 * 删除
+	 * @param id
+	 * @return
+	 */
+	public Ret delete(Long id) {
+		return deleteById(id,true);
+	}
+
+	/**
+	 * 删除数据后执行的回调
+	 * @param annualorderdQty 要删除的model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	protected String afterDelete(AnnualorderdQty annualorderdQty, Kv kv) {
+		//addDeleteSystemLog(annualorderdQty.getIautoid(), JBoltUserKit.getUserId(),annualorderdQty.getName());
+		return null;
+	}
+
+	/**
+	 * 检测是否可以删除
+	 * @param annualorderdQty 要删除的model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkCanDelete(AnnualorderdQty annualorderdQty, Kv kv) {
+		//如果检测被用了 返回信息 则阻止删除 如果返回null 则正常执行删除
+		return checkInUse(annualorderdQty, kv);
+	}
+
+	/**
+	 * 设置返回二开业务所属的关键systemLog的targetType 
+	 * @return
+	 */
+	@Override
+	protected int systemLogTargetType() {
+		return ProjectSystemLogTargetType.NONE.getValue();
+	}
+
+	/**
+	 * 切换isdeleted属性
+	 */
+	public Ret toggleIsDeleted(Long id) {
+		return toggleBoolean(id, "isDeleted");
+	}
+
+	/**
+	 * 检测是否可以toggle操作指定列
+	 * @param annualorderdQty 要toggle的model
+	 * @param column 操作的哪一列
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkCanToggle(AnnualorderdQty annualorderdQty,String column, Kv kv) {
+		//没有问题就返回null  有问题就返回提示string 字符串
+		return null;
+	}
+
+	/**
+	 * toggle操作执行后的回调处理
+	 */
+	@Override
+	protected String afterToggleBoolean(AnnualorderdQty annualorderdQty, String column, Kv kv) {
+		//addUpdateSystemLog(annualorderdQty.getIautoid(), JBoltUserKit.getUserId(), annualorderdQty.getName(),"的字段["+column+"]值:"+annualorderdQty.get(column));
+		return null;
+	}
+
+	/**
+	 * 检测是否可以删除
+	 * @param annualorderdQty model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkInUse(AnnualorderdQty annualorderdQty, Kv kv) {
+		//这里用来覆盖 检测AnnualorderdQty是否被其它表引用
+		return null;
+	}
+
+}
