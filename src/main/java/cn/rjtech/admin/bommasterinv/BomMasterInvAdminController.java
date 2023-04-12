0cadf984fdb6204cb613ad1f28ac895c4409d276
timi <246151394592qq.com>
2023-04-12 10:22:53 +0800

    【调整】 1：生成基础档案-母件物料存货集。 2：物料子件按编码排序

---
+++ src/main/java/cn/rjtech/admin/bommasterinv/BomMasterInvAdminController.java
@@ -0,0 +1,88 @@
+package cn.rjtech.admin.bommasterinv;
+
+import com.jfinal.aop.Inject;
+import cn.rjtech.base.controller.BaseAdminController;
+import cn.jbolt.core.permission.CheckPermission;
+import cn.jbolt._admin.permission.PermissionKey;
+import com.jfinal.core.Path;
+import com.jfinal.aop.Before;
+import cn.jbolt._admin.interceptor.JBoltAdminAuthInterceptor;
+import com.jfinal.plugin.activerecord.tx.Tx;
+import cn.jbolt.core.base.JBoltMsg;
+import cn.rjtech.model.momdata.BomMasterInv;
+/**
+ * 基础档案-母件物料存货集（新增/修改版本变更时处理，定时处理更新）
+ * @ClassName: BomMasterInvAdminController
+ * @author: 佛山市瑞杰科技有限公司
+ * @date: 2023-04-12 10:19
+ */
+@Before(JBoltAdminAuthInterceptor.class)
+@Path(value = "/admin/bommasterinv", viewPath = "/_view/admin/bommasterinv")
+public class BomMasterInvAdminController extends BaseAdminController {
+
+	@Inject
+	private BomMasterInvService service;
+
+   /**
+	* 首页
+	*/
+	public void index() {
+		render("index.html");
+	}
+   /**
+	* 数据源
+	*/
+	public void datas() {
+		renderJsonData(service.getAdminDatas(getPageNumber(), getPageSize()));
+	}
+
+   /**
+	* 新增
+	*/
+	public void add() {
+		render("add.html");
+	}
+
+   /**
+	* 保存
+	*/
+	public void save() {
+		renderJson(service.save(getModel(BomMasterInv.class, "bomMasterInv")));
+	}
+
+   /**
+	* 编辑
+	*/
+	public void edit() {
+		BomMasterInv bomMasterInv=service.findById(getLong(0));
+		if(bomMasterInv == null){
+			renderFail(JBoltMsg.DATA_NOT_EXIST);
+			return;
+		}
+		set("bomMasterInv",bomMasterInv);
+		render("edit.html");
+	}
+
+   /**
+	* 更新
+	*/
+	public void update() {
+		renderJson(service.update(getModel(BomMasterInv.class, "bomMasterInv")));
+	}
+
+   /**
+	* 批量删除
+	*/
+	public void deleteByIds() {
+		renderJson(service.deleteByIds(get("ids")));
+	}
+
+   /**
+	* 删除
+	*/
+	public void delete() {
+		renderJson(service.deleteById(getLong(0)));
+	}
+
+
+}
