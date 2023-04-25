3d5112a7401b8778005de7294f00d6c5a162df46
timi <246151394592qq.com>
2023-04-25 20:08:44 +0800

    【调整】   1.采购订单代码提交

--- src/main/java/cn/rjtech/admin/purchaseorderdbatchversion/PurchaseOrderDBatchVersionService.java
+++ src/main/java/cn/rjtech/admin/purchaseorderdbatchversion/PurchaseOrderDBatchVersionService.java
@@ -1,14 +1,19 @@
 package cn.rjtech.admin.purchaseorderdbatchversion;
 
-import com.jfinal.plugin.activerecord.Page;
-import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
-import cn.jbolt.core.service.base.BaseService;
-import com.jfinal.kit.Kv;
-import com.jfinal.kit.Okv;
-import com.jfinal.kit.Ret;
+import cn.hutool.core.date.DateUtil;
 import cn.jbolt.core.base.JBoltMsg;
 import cn.jbolt.core.db.sql.Sql;
+import cn.jbolt.core.kit.JBoltUserKit;
+import cn.jbolt.core.service.base.BaseService;
+import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
 import cn.rjtech.model.momdata.PurchaseOrderDBatchVersion;
+import com.jfinal.kit.Kv;
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Page;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 /**
  * 采购/委外管理-采购现品票版本记录
  * @ClassName: PurchaseOrderDBatchVersionService
@@ -106,5 +111,23 @@ public class PurchaseOrderDBatchVersionService extends BaseService<PurchaseOrder
 		//这里用来覆盖 检测是否被其它表引用
 		return null;
 	}
-
+	
+	public PurchaseOrderDBatchVersion createBatchVersion(Long purchaseOrderMId, Long batchId, Long	inventoryId, Date planDate, String version, String sourceVersion, String barCode, String sourceBarCode, BigDecimal qty, BigDecimal sourceQty){
+		PurchaseOrderDBatchVersion purchaseOrderDBatchVersion = new PurchaseOrderDBatchVersion();
+		
+		purchaseOrderDBatchVersion.setIPurchaseOrderMid(purchaseOrderMId);
+		purchaseOrderDBatchVersion.setIPurchaseOrderdBatchId(batchId);
+		purchaseOrderDBatchVersion.setIInventoryId(inventoryId);
+		purchaseOrderDBatchVersion.setDPlanDate(planDate);
+		purchaseOrderDBatchVersion.setCVersion(version);
+		purchaseOrderDBatchVersion.setCSourceVersion(sourceVersion);
+		purchaseOrderDBatchVersion.setCBarcode(barCode);
+		purchaseOrderDBatchVersion.setCSourceBarcode(sourceBarCode);
+		purchaseOrderDBatchVersion.setIQty(qty);
+		purchaseOrderDBatchVersion.setISourceQty(sourceQty);
+		purchaseOrderDBatchVersion.setICreateBy(JBoltUserKit.getUserId());
+		purchaseOrderDBatchVersion.setCCreateName(JBoltUserKit.getUserName());
+		purchaseOrderDBatchVersion.setDCreateTime(DateUtil.date());
+		return purchaseOrderDBatchVersion;
+	}
 }
