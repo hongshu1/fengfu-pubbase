3194916265601ec38ff119df3377a899fd56d055
cc <1969299442@qq.com>
2023-05-18 14:10:04 +0800

    拉取代码

--- src/main/java/cn/rjtech/admin/codingrulem/CodingRuleMAdminController.java
+++ src/main/java/cn/rjtech/admin/codingrulem/CodingRuleMAdminController.java
@@ -5,13 +5,19 @@ import cn.jbolt.core.base.JBoltMsg;
 import cn.jbolt.core.permission.CheckPermission;
 import cn.jbolt.core.permission.JBoltAdminAuthInterceptor;
 import cn.jbolt.core.permission.UnCheckIfSystemAdmin;
+import cn.rjtech.admin.form.FormService;
 import cn.rjtech.base.controller.BaseAdminController;
 import cn.rjtech.model.momdata.CodingRuleM;
+import cn.rjtech.model.momdata.Form;
+import cn.rjtech.util.ValidationUtils;
 import com.jfinal.aop.Before;
 import com.jfinal.aop.Inject;
 import com.jfinal.core.Path;
+import com.jfinal.template.stat.ast.For;
+
 /**
  * 系统配置-编码规则
+ *
  * @ClassName: CodingRuleMAdminController
  * @author: 佛山市瑞杰科技有限公司
  * @date: 2023-05-04 14:00
@@ -22,69 +28,71 @@ import com.jfinal.core.Path;
 @Path(value = "/admin/codingrulem", viewPath = "/_view/admin/codingrulem")
 public class CodingRuleMAdminController extends BaseAdminController {
 
-	@Inject
-	private CodingRuleMService service;
+    @Inject
+    private CodingRuleMService service;
 
-   /**
-	* 首页
-	*/
-	public void index() {
-		render("index.html");
-	}
-   /**
-	* 数据源
-	*/
-	public void datas() {
-		renderJsonData(service.getAdminDatas(getPageNumber(), getPageSize(), getKeywords(), get("cFormTypeSn"), getInt("iCodingType"), getBoolean("IsDeleted")));
-	}
+    /**
+     * 首页
+     */
+    public void index() {
+        render("index.html");
+    }
 
-   /**
-	* 新增
-	*/
-	public void add() {
-		render("add.html");
-	}
+    /**
+     * 数据源
+     */
+    public void datas() {
+        renderJsonData(service.getAdminDatas(getPageNumber(), getPageSize(), getKeywords(), get("cFormTypeSn"), getInt("iCodingType"), getBoolean("IsDeleted")));
+    }
 
-   /**
-	* 保存
-	*/
-	public void save() {
-		renderJson(service.save(getModel(CodingRuleM.class, "codingRuleM"),get("iFormId")));
-	}
+    /**
+     * 新增
+     */
+    public void add() {
+        render("add.html");
+    }
 
-   /**
-	* 编辑
-	*/
-	public void edit() {
-		CodingRuleM codingRuleM=service.findById(getLong(0));
-		if(codingRuleM == null){
-			renderFail(JBoltMsg.DATA_NOT_EXIST);
-			return;
-		}
-		set("codingRuleM",codingRuleM);
-		render("edit.html");
-	}
+    /**
+     * 保存
+     */
+    public void save() {
+        renderJson(service.save(getModel(CodingRuleM.class, "codingRuleM"), get("iFormId")));
+    }
 
-   /**
-	* 更新
-	*/
-	public void update() {
-		renderJson(service.update(getModel(CodingRuleM.class, "codingRuleM")));
-	}
+    /**
+     * 编辑
+     */
+    public void edit() {
+        CodingRuleM codingRuleM = service.findById(getLong(0));
+        if (codingRuleM == null) {
+            renderFail(JBoltMsg.DATA_NOT_EXIST);
+            return;
+        }
 
-   /**
-	* 批量删除
-	*/
-	public void deleteByIds() {
-		renderJson(service.deleteByIds(get("ids")));
-	}
+        set("codingRuleM", codingRuleM);
+        render("edit.html");
+    }
 
-   /**
-	* 删除
-	*/
-	public void delete() {
-		renderJson(service.deleteById(getLong(0)));
-	}
+    /**
+     * 更新
+     */
+    public void update() {
+        renderJson(service.update(getModel(CodingRuleM.class, "codingRuleM")));
+    }
+
+    /**
+     * 批量删除
+     */
+    public void deleteByIds() {
+        renderJson(service.deleteByIds(get("ids")));
+    }
+
+    /**
+     * 删除
+     */
+    public void delete() {
+        renderJson(service.deleteById(getLong(0)));
+    }
 
     public void saveTableSubmit() {
         renderJson(service.saveTableSubmit(getJBoltTable()));
