77522fdfba76a56846d13a0f897d88c5891b9d83
Kephon <kaifanlai@qq.com>
2023-05-18 14:01:11 +0800

    【调整】支持从指定的组织ERP数据源执行查询

--- src/main/java/cn/rjtech/common/CommonController.java
+++ src/main/java/cn/rjtech/common/CommonController.java
@@ -3,6 +3,7 @@ package cn.rjtech.common;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import cn.jbolt.core.api.JBoltApplyJWT;
+import cn.jbolt.core.kit.U8DataSourceKit;
 import cn.jbolt.core.util.JBoltStringUtil;
 import cn.rjtech.app.user.service.UserAppService;
 import cn.rjtech.base.controller.BaseRestController;
@@ -22,7 +23,6 @@ import com.jfinal.kit.Kv;
 import com.jfinal.plugin.activerecord.Record;
 
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 
@@ -65,7 +65,11 @@ public class CommonController extends BaseRestController {
         }
 
         Organize orgApp = organizeService.getOrgByCode(organizeCode);
-        Record userApp = userAppService.getUserByUserCode(orgApp.getErpdbname(), orgApp.getErpdbschemas(), userCode);
+
+        // 对U8数据源别名做特殊处理，如果是u8开头的数据源别名，结合组织编码定义数据源别名，如u8001
+        String erpDbAlias = StrUtil.startWith(orgApp.getErpdbalias(), "u8") ? U8DataSourceKit.ME.use(orgApp.getOrganizecode()) : orgApp.getErpdbalias();
+
+        Record userApp = userAppService.getUserByUserCode(erpDbAlias, orgApp.getErpdbname(), orgApp.getErpdbschemas(), userCode);
         if (null == userApp) {
             renderJsonFail("请重新登录！");
             return;
@@ -109,7 +113,7 @@ public class CommonController extends BaseRestController {
      * 单据提交
      */
     @JBoltApplyJWT
-    public void vouchProcessSubmit(){
+    public void vouchProcessSubmit() {
         Map map = commonService.vouchProcessSubmit(getKv());
         if ("200".equals(map.get("code").toString())) {
             renderJsonSuccess(map);
@@ -123,16 +127,18 @@ public class CommonController extends BaseRestController {
      * erp/common/getBillInfo
      */
     @JBoltApplyJWT
-    public void getBillInfo(){
+    public void getBillInfo() {
         Kv kv = getKv();
+
         String barCode = kv.getStr("barCode");
         String vouchCode = kv.getStr("vouchCode");
         String organizeCode = kv.getStr("organizeCode");
-        Organize orgApp = organizeService.getOrgByCode(kv.getStr("organizeCode"));
+
+        Organize orgApp = organizeService.getOrgByCode(organizeCode);
+
         List<Record> list = commonService.findInfoFlag(vouchCode);
-        Map<String, String> map = new Hashtable<>();
-        List<Map<String,Object>> record = commonService.getBillInfo(orgApp, barCode, list.get(0).getStr("billinfoflag"));
-        Map data = new HashMap();
+        List<Map<String, Object>> record = commonService.getBillInfo(orgApp, barCode, list.get(0).getStr("billinfoflag"));
+        Map<String, Object> data = new HashMap<>();
         data.put("detail", record);
         renderJsonSuccess(data);
     }
@@ -142,7 +148,7 @@ public class CommonController extends BaseRestController {
      * erp/common/getBarcodeInfo
      */
     @JBoltApplyJWT
-    public void getBarcodeInfo(){
+    public void getBarcodeInfo() {
         Kv kv = getKv();
         String barCode = kv.getStr("barCode");
         String vouchCode = kv.getStr("vouchCode");
@@ -150,31 +156,35 @@ public class CommonController extends BaseRestController {
         String infoFlag = kv.getStr("infoFlag");
         String billno = kv.getStr("billno");
         String organizeCode = kv.getStr("organizeCode");
+
         Organize orgApp = organizeService.getOrgByCode(organizeCode);
-        String barcodeInfoFlag = infoFlag;
-        Map data = new HashMap();
-        if(vouchCode.length() != 0 ) {
+
+        String barcodeInfoFlag;
+
+        Map<String, Object> data = new HashMap<>();
+
+        if (vouchCode.length() != 0) {
             List<Record> list = commonService.findInfoFlag(vouchCode);
             barcodeInfoFlag = list.get(0).getStr("barcodeinfoflag");
-            if (JBoltStringUtil.isNotBlank(barcodeInfoFlag) && barcodeInfoFlag.indexOf("@") >= 0) {
+            if (JBoltStringUtil.isNotBlank(barcodeInfoFlag) && barcodeInfoFlag.contains("@")) {
                 //替换送货单条码
-                if (barcodeInfoFlag.indexOf("@billNo") >= 0) {
+                if (barcodeInfoFlag.contains("@billNo")) {
                     barcodeInfoFlag = barcodeInfoFlag.replace("@billNo", billno);
                 }
             }
             List<Map<String, Object>> barcodeInfo = commonService.getBarcodeInfo(orgApp, barCode, posCode, barcodeInfoFlag);
-            data.put("detail",barcodeInfo);
+            data.put("detail", barcodeInfo);
         }
         //commonService.getBarcodeInfo();
         renderJsonSuccess(data);
     }
 
     @JBoltApplyJWT
-    public void getVouchTypeList(){
+    public void getVouchTypeList() {
         Kv kv = getKv();
         String vouchCode = kv.getStr("vouchCode");
         Record record = commonService.findInfoFlag(vouchCode).get(0);
-        Map data = new HashMap();
+        Map<String, Object> data = new HashMap<>();
         data.put("detail", record);
         renderJsonSuccess(data);
     }
@@ -184,8 +194,8 @@ public class CommonController extends BaseRestController {
      * erp/common/findComponentList
      */
     @JBoltApplyJWT
-    public void findComponentList(){
-        Map result =new HashMap();
+    public void findComponentList() {
+        Map<String, Object> result = new HashMap<>();
         Kv kv = getKv();
         String type = kv.getStr("type");//类型
         String queryCode = kv.getStr("queryCode");//编码
@@ -193,34 +203,36 @@ public class CommonController extends BaseRestController {
         String queryName = kv.getStr("queryName");
         String extData = kv.getStr("extData");
         String flag = kv.getStr("flag");
-        if (JBoltStringUtil.isNotBlank(queryName)){
-            queryID=queryName;
+        if (JBoltStringUtil.isNotBlank(queryName)) {
+            queryID = queryName;
         }
         ValidationUtils.notBlank(type, "请检查传入的类型！");
         Map<String, Object> generalQuerySeting = commonService.generalQuerySeting(type);
         ValidationUtils.notNull(generalQuerySeting, "当前类型未配置！");
 
         Organize orgApp = organizeService.getOrgByCode(kv.getStr("organizeCode"));
-        Map paramMap=new HashMap();
-        paramMap.put("erpDBName",orgApp.getErpdbname());
-        paramMap.put("erpDBSchemas",orgApp.getErpdbschemas());
-        paramMap.put("processName",generalQuerySeting.get("process").toString());
-        paramMap.put("OrganizeCode",orgApp.getOrganizecode());
-        paramMap.put("QueryCode",queryCode);
-        paramMap.put("QueryID",queryID);
-        paramMap.put("ExtData",extData);
-        paramMap.put("Flag",flag);
-        paramMap.put("erpdbalias",orgApp.getErpdbalias());
-        paramMap.put("ResultCode","resultCode");
-        paramMap.put("ResultInfo","resultInfo");
 
-        Map map = commonService.findComponentList(paramMap);
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("erpDBName", orgApp.getErpdbname());
+        paramMap.put("erpDBSchemas", orgApp.getErpdbschemas());
+        paramMap.put("processName", generalQuerySeting.get("process").toString());
+        paramMap.put("OrganizeCode", orgApp.getOrganizecode());
+        paramMap.put("QueryCode", queryCode);
+        paramMap.put("QueryID", queryID);
+        paramMap.put("ExtData", extData);
+        paramMap.put("Flag", flag);
+        paramMap.put("erpdbalias", orgApp.getErpdbalias());
+        paramMap.put("ResultCode", "resultCode");
+        paramMap.put("ResultInfo", "resultInfo");
+
+        Map<String, Object> map = commonService.findComponentList(paramMap);
         String dataStr = JSONObject.toJSONString(map.get("list"));
         System.out.println(dataStr);
+
         JsonElement jsonElement = JsonParser.parseString(dataStr);
         List<Map<String, Object>> maps = DataConversion.toListMap(jsonElement.toString());
-        result.put("detail",maps);
-        result.put("ColumnList",paramMap.get("ResultInfo").toString());
+        result.put("detail", maps);
+        result.put("ColumnList", paramMap.get("ResultInfo").toString());
         renderJsonSuccess(result);
     }
 
