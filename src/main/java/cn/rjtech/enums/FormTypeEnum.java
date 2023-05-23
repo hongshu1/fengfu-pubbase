36a5ecf433280f56d7b06d57c2eab9a7aef98543
timi <246151394592qq.com>
2023-05-23 22:55:33 +0800

    【调整】 1.编码规则配置代码提交

---
+++ src/main/java/cn/rjtech/enums/FormTypeEnum.java
@@ -0,0 +1,44 @@
+package cn.rjtech.enums;
+
+import java.util.*;
+
+public enum FormTypeEnum {
+    
+    documentType("1", "单据类型"),
+    barCodeType("2", "条码号");
+    private static final Map<String, FormTypeEnum> FORM_TYPE_ENUM_MAP = new HashMap<>();
+    
+    static {
+        for (FormTypeEnum formTypeEnum : FormTypeEnum.values()) {
+            FORM_TYPE_ENUM_MAP.put(formTypeEnum.value, formTypeEnum);
+        }
+    }
+    
+    private final String value;
+    private final String text;
+    
+    FormTypeEnum(String value, String text) {
+        this.value = value;
+        this.text  = text;
+    }
+    
+    public static FormTypeEnum toEnum(String value) {
+        return FORM_TYPE_ENUM_MAP.get(value);
+    }
+    
+    @Override
+    public String toString() {
+        return "FormTypeEnum{" +
+                "value=" + value +
+                ", text='" + text + '\'' +
+                '}';
+    }
+    
+    public String getValue() {
+        return value;
+    }
+    
+    public String getText() {
+        return text;
+    }
+}
