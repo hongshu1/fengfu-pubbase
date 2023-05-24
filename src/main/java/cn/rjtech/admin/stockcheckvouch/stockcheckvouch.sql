9b7bc2884a8fdc92491ceb4cb7e0278567a661af
cc <1969299442@qq.com>
2023-05-24 08:58:14 +0800

    更新【库存管理】

--- src/main/java/cn/rjtech/admin/stockcheckvouch/stockcheckvouch.sql
+++ src/main/java/cn/rjtech/admin/stockcheckvouch/stockcheckvouch.sql
@@ -1,8 +1,9 @@
 #sql("datas")
-select t1.*,t2.cWhName
+select t1.*,t2.cWhName,t3.cPsn_Name
 from T_Sys_StockCheckVouch t1
 left join Bd_Warehouse t2 on t1.WhCode = t2.cWhCode
-where 1=1
+left join bd_person t3 on t1.checkperson = t3.iautoid
+     where 1=1
 #if(billno)
   and t1.BillNo like CONCAT('%', #para(billno), '%')
 #end
