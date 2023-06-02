7ed8b38f8d070c87228d31bccf1cd9252b7da95b
谢亮亮 <3328606122@qq.com>
2023-06-02 15:58:14 +0800

    委外订单订货清单表格数据处理(未完成)

--- src/main/java/cn/rjtech/admin/subcontractorderdbatch/subcontractorderdbatch.sql
+++ src/main/java/cn/rjtech/admin/subcontractorderdbatch/subcontractorderdbatch.sql
@@ -1,6 +1,7 @@
 #sql("findBySubcontractOrderMId")
 SELECT
     ROW_NUMBER() OVER (ORDER BY inv.cInvCode) AS SequenceNumber,
+    derm.cOrderNo,
 	inv.cInvCode,
 	inv.cInvName,
 	inv.cInvCode1,
@@ -9,6 +10,7 @@ SELECT
 FROM
 	PS_SubcontractOrderDBatch a
 	INNER JOIN PS_SubcontractOrderD d ON d.iAutoId = a.iSubcontractOrderDid
+	LEFT JOIN PS_SubcontractOrderM derm ON derm.iAutoId= d.iSubcontractOrderMid
 	INNER JOIN Bd_Inventory inv ON inv.iAutoId = a.iinventoryId
 WHERE
 	d.isDeleted = 0
