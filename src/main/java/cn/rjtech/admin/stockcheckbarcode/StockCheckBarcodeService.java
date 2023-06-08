ed363070489f5de60bde8603c5f8d551fe3ce245
cc <1969299442@qq.com>
2023-06-08 22:53:25 +0800

    修复【采购入库单】+生成库存盘点的相关model、controller

---
+++ src/main/java/cn/rjtech/admin/stockcheckbarcode/StockCheckBarcodeService.java
@@ -0,0 +1,169 @@
+package cn.rjtech.admin.stockcheckbarcode;
+
+import com.jfinal.plugin.activerecord.Page;
+import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
+import cn.jbolt.core.service.base.BaseService;
+import com.jfinal.kit.Kv;
+import com.jfinal.kit.Okv;
+import com.jfinal.kit.Ret;
+import com.jfinal.plugin.activerecord.Db;
+import cn.jbolt.core.base.JBoltMsg;
+import cn.rjtech.model.momdata.StockCheckBarcode;
+/**
+ * 库存盘点-条码明细 Service
+ * @ClassName: StockCheckBarcodeService
+ * @author: 佛山市瑞杰科技有限公司
+ * @date: 2023-06-08 22:47
+ */
+public class StockCheckBarcodeService extends BaseService<StockCheckBarcode> {
+
+	private final StockCheckBarcode dao = new StockCheckBarcode().dao();
+
+	@Override
+	protected StockCheckBarcode dao() {
+		return dao;
+	}
+
+	/**
+	 * 后台管理分页查询
+	 * @param pageNumber
+	 * @param pageSize
+	 * @param keywords
+	 * @return
+	 */
+	public Page<StockCheckBarcode> paginateAdminDatas(int pageNumber, int pageSize, String keywords) {
+		return paginateByKeywords("CreateDate","DESC", pageNumber, pageSize, keywords, "iAutoId");
+	}
+
+	/**
+	 * 保存
+	 * @param stockCheckBarcode
+	 * @return
+	 */
+	public Ret save(StockCheckBarcode stockCheckBarcode) {
+		if(stockCheckBarcode==null || isOk(stockCheckBarcode.getAutoID())) {
+			return fail(JBoltMsg.PARAM_ERROR);
+		}
+		//if(existsName(stockCheckBarcode.getName())) {return fail(JBoltMsg.DATA_SAME_NAME_EXIST);}
+		boolean success=stockCheckBarcode.save();
+		if(success) {
+			//添加日志
+			//addSaveSystemLog(stockCheckBarcode.getAutoID(), JBoltUserKit.getUserId(), stockCheckBarcode.getName());
+		}
+		return ret(success);
+	}
+
+	/**
+	 * 更新
+	 * @param stockCheckBarcode
+	 * @return
+	 */
+	public Ret update(StockCheckBarcode stockCheckBarcode) {
+		if(stockCheckBarcode==null || notOk(stockCheckBarcode.getAutoID())) {
+			return fail(JBoltMsg.PARAM_ERROR);
+		}
+		//更新时需要判断数据存在
+		StockCheckBarcode dbStockCheckBarcode=findById(stockCheckBarcode.getAutoID());
+		if(dbStockCheckBarcode==null) {return fail(JBoltMsg.DATA_NOT_EXIST);}
+		//if(existsName(stockCheckBarcode.getName(), stockCheckBarcode.getAutoID())) {return fail(JBoltMsg.DATA_SAME_NAME_EXIST);}
+		boolean success=stockCheckBarcode.update();
+		if(success) {
+			//添加日志
+			//addUpdateSystemLog(stockCheckBarcode.getAutoID(), JBoltUserKit.getUserId(), stockCheckBarcode.getName());
+		}
+		return ret(success);
+	}
+
+	/**
+	 * 删除 指定多个ID
+	 * @param ids
+	 * @return
+	 */
+	public Ret deleteByBatchIds(String ids) {
+		return deleteByIds(ids,true);
+	}
+
+	/**
+	 * 删除
+	 * @param id
+	 * @return
+	 */
+	public Ret delete(Long id) {
+		return deleteById(id,true);
+	}
+
+	/**
+	 * 删除数据后执行的回调
+	 * @param stockCheckBarcode 要删除的model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	protected String afterDelete(StockCheckBarcode stockCheckBarcode, Kv kv) {
+		//addDeleteSystemLog(stockCheckBarcode.getAutoID(), JBoltUserKit.getUserId(),stockCheckBarcode.getName());
+		return null;
+	}
+
+	/**
+	 * 检测是否可以删除
+	 * @param stockCheckBarcode 要删除的model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkCanDelete(StockCheckBarcode stockCheckBarcode, Kv kv) {
+		//如果检测被用了 返回信息 则阻止删除 如果返回null 则正常执行删除
+		return checkInUse(stockCheckBarcode, kv);
+	}
+
+	/**
+	 * 设置返回二开业务所属的关键systemLog的targetType 
+	 * @return
+	 */
+	@Override
+	protected int systemLogTargetType() {
+		return ProjectSystemLogTargetType.NONE.getValue();
+	}
+
+	/**
+	 * 切换isdeleted属性
+	 */
+	public Ret toggleIsDeleted(Long id) {
+		return toggleBoolean(id, "isDeleted");
+	}
+
+	/**
+	 * 检测是否可以toggle操作指定列
+	 * @param stockCheckBarcode 要toggle的model
+	 * @param column 操作的哪一列
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkCanToggle(StockCheckBarcode stockCheckBarcode,String column, Kv kv) {
+		//没有问题就返回null  有问题就返回提示string 字符串
+		return null;
+	}
+
+	/**
+	 * toggle操作执行后的回调处理
+	 */
+	@Override
+	protected String afterToggleBoolean(StockCheckBarcode stockCheckBarcode, String column, Kv kv) {
+		//addUpdateSystemLog(stockCheckBarcode.getAutoID(), JBoltUserKit.getUserId(), stockCheckBarcode.getName(),"的字段["+column+"]值:"+stockCheckBarcode.get(column));
+		return null;
+	}
+
+	/**
+	 * 检测是否可以删除
+	 * @param stockCheckBarcode model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkInUse(StockCheckBarcode stockCheckBarcode, Kv kv) {
+		//这里用来覆盖 检测StockCheckBarcode是否被其它表引用
+		return null;
+	}
+
+}
