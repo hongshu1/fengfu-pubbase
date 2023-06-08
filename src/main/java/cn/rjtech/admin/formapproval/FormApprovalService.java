d721dd9ed8c330cea5ca3b3f068088d6cf2d6c65
koden <719053257@qq.com>
2023-06-08 16:49:10 +0800

    审批进度方法

--- src/main/java/cn/rjtech/admin/formapproval/FormApprovalService.java
+++ src/main/java/cn/rjtech/admin/formapproval/FormApprovalService.java
@@ -1157,10 +1157,12 @@ public class FormApprovalService extends BaseService<FormApproval> {
                 flowD.update();
 
 //                反审后 单据状态都是待审批
-                updateSql().update(formSn)
-                        .set(IAUDITSTATUS, AuditStatusEnum.AWAIT_AUDIT.getValue())
-                        .set(DAUDITTIME, now)
-                        .eq(IAUTOID, formAutoId);
+                    Sql updateSql = updateSql().update(formSn)
+                            .set(IAUDITSTATUS, AuditStatusEnum.AWAIT_AUDIT.getValue())
+                            .set(DAUDITTIME, now)
+                            .eq(IAUTOID, formAutoId);
+
+                    update(updateSql);
 
                     return true;
                 });
@@ -1437,5 +1439,45 @@ public class FormApprovalService extends BaseService<FormApproval> {
         }
 
         return SUCCESS;
+    }
+
+    /**
+     * 判断审核还是审批
+     * @param formSn
+     * @return
+     */
+    public Ret auditOrApprove(String formSn){
+
+        AuditFormConfig formConfig = auditFormConfigService.findFirst("select * from Bd_AuditFormConfig where IsDeleted = '0' and isEnabled = '1' " +
+                "and cFormSn = '" + formSn + "'");
+
+        Integer iType = 0;
+
+        if (isOk(formConfig)){
+            iType = formConfig.getIType();
+        }
+
+        return SUCCESS.set("iType",iType);
+    }
+
+    /**
+     * 查询审批过程待审批的人员
+     * @param formAutoId
+     * @return
+     */
+    public String approvalProcessUsers(Long formAutoId,Integer size){
+        Kv kv = new Kv();
+        kv.set("formAutoId", isOk(formAutoId) ? formAutoId : ' ');
+        kv.set("size", isOk(size) ? size : 5);
+        String string = new String();
+        List<Record> list = dbTemplate("formapproval.approvalProcessUsers", kv).find();
+        StringBuilder builder = new StringBuilder();
+        if (list.size() > 0) {
+            list.forEach(record -> {
+                builder.append(record.getStr("name")).append(",");
+            });
+            string = builder.substring(1, builder.length() - 1);
+        }
+        return string;
     }
 }
