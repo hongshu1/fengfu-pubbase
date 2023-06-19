715d087e26f50d92094831b07e45740aad33b09b
王统 <1374030696@qq.com>
2023-06-19 16:28:20 +0800

    完善备料扫码

---
+++ src/main/java/cn/rjtech/admin/sysmaterialspreparescan/SysMaterialspreparescanService.java
@@ -0,0 +1,108 @@
+package cn.rjtech.admin.sysmaterialspreparescan;
+
+import com.jfinal.plugin.activerecord.Page;
+import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
+import cn.jbolt.core.service.base.BaseService;
+import com.jfinal.kit.Kv;
+import com.jfinal.kit.Okv;
+import com.jfinal.kit.Ret;
+import cn.jbolt.core.base.JBoltMsg;
+import cn.jbolt.core.db.sql.Sql;
+import cn.rjtech.model.momdata.SysMaterialspreparescan;
+/**
+ * null管理
+ * @ClassName: SysMaterialspreparescanService
+ * @author: 佛山市瑞杰科技有限公司
+ * @date: 2023-06-17 17:36
+ */
+public class SysMaterialspreparescanService extends BaseService<SysMaterialspreparescan> {
+	private final SysMaterialspreparescan dao=new SysMaterialspreparescan().dao();
+	@Override
+	protected SysMaterialspreparescan dao() {
+		return dao;
+	}
+
+	@Override
+    protected int systemLogTargetType() {
+        return ProjectSystemLogTargetType.NONE.getValue();
+    }
+
+	/**
+	 * 后台管理数据查询
+	 * @param pageNumber 第几页
+	 * @param pageSize   每页几条数据
+     * @param State 状态：1=已扫描，0=未扫描
+	 * @return
+	 */
+	public Page<SysMaterialspreparescan> getAdminDatas(int pageNumber, int pageSize, String State) {
+	    //创建sql对象
+	    Sql sql = selectSql().page(pageNumber,pageSize);
+	    //sql条件处理
+        sql.eq("State",State);
+        //排序
+        sql.desc("iAutoId");
+		return paginate(sql);
+	}
+
+	/**
+	 * 保存
+	 * @param sysMaterialspreparescan
+	 * @return
+	 */
+	public Ret save(SysMaterialspreparescan sysMaterialspreparescan) {
+		if(sysMaterialspreparescan==null || isOk(sysMaterialspreparescan.getIAutoId())) {
+			return fail(JBoltMsg.PARAM_ERROR);
+		}
+		boolean success=sysMaterialspreparescan.save();
+		if(success) {
+			//添加日志
+			//addSaveSystemLog(sysMaterialspreparescan.getIAutoId(), JBoltUserKit.getUserId(), sysMaterialspreparescan.getName());
+		}
+		return ret(success);
+	}
+
+	/**
+	 * 更新
+	 * @param sysMaterialspreparescan
+	 * @return
+	 */
+	public Ret update(SysMaterialspreparescan sysMaterialspreparescan) {
+		if(sysMaterialspreparescan==null || notOk(sysMaterialspreparescan.getIAutoId())) {
+			return fail(JBoltMsg.PARAM_ERROR);
+		}
+		//更新时需要判断数据存在
+		SysMaterialspreparescan dbSysMaterialspreparescan=findById(sysMaterialspreparescan.getIAutoId());
+		if(dbSysMaterialspreparescan==null) {return fail(JBoltMsg.DATA_NOT_EXIST);}
+		boolean success=sysMaterialspreparescan.update();
+		if(success) {
+			//添加日志
+			//addUpdateSystemLog(sysMaterialspreparescan.getIAutoId(), JBoltUserKit.getUserId(), sysMaterialspreparescan.getName());
+		}
+		return ret(success);
+	}
+
+	/**
+	 * 删除数据后执行的回调
+	 * @param sysMaterialspreparescan 要删除的model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	protected String afterDelete(SysMaterialspreparescan sysMaterialspreparescan, Kv kv) {
+		//addDeleteSystemLog(sysMaterialspreparescan.getIAutoId(), JBoltUserKit.getUserId(),sysMaterialspreparescan.getName());
+		return null;
+	}
+
+	/**
+	 * 检测是否可以删除
+	 * @param sysMaterialspreparescan model
+	 * @param kv 携带额外参数一般用不上
+	 * @return
+	 */
+	@Override
+	public String checkInUse(SysMaterialspreparescan sysMaterialspreparescan, Kv kv) {
+		//这里用来覆盖 检测是否被其它表引用
+		return null;
+	}
+
+}
