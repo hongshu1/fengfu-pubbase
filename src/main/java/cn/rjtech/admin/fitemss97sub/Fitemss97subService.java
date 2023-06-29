ef8d7cf95aae76ae78ac7bc5e3bd7e3a9e9326a3
hzx <ytlk_hzx@163.com>
2023-06-29 22:39:37 +0800

    基础档案-项目档案

--- src/main/java/cn/rjtech/admin/fitemss97sub/Fitemss97subService.java
+++ src/main/java/cn/rjtech/admin/fitemss97sub/Fitemss97subService.java
@@ -1,18 +1,24 @@
 package cn.rjtech.admin.fitemss97sub;
 
+import cn.hutool.core.util.ObjUtil;
 import cn.jbolt.core.base.JBoltMsg;
+import cn.jbolt.core.bean.JsTreeBean;
 import cn.jbolt.core.kit.JBoltUserKit;
 import cn.jbolt.core.model.User;
 import cn.jbolt.core.service.base.BaseService;
 import cn.jbolt.extend.systemlog.ProjectSystemLogTargetType;
 import cn.rjtech.enums.SourceEnum;
 import cn.rjtech.model.momdata.Fitemss97sub;
+import cn.rjtech.util.ValidationUtils;
 import com.jfinal.kit.Kv;
 import com.jfinal.kit.Ret;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
 
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 项目管理大类项目子目录 Service
  * @ClassName: Fitemss97subService
@@ -32,13 +38,124 @@ public class Fitemss97subService extends BaseService<Fitemss97sub> {
 	 * 后台管理分页查询
 	 * @param pageNumber
 	 * @param pageSize
-	 * @param keywords
+	 * @param
 	 * @return
 	 */
 	public Page<Record> paginateAdminDatas(int pageNumber, int pageSize, Kv para) {
-		para.set("iorgid",getOrgId());
-		return dbTemplate("fitemss97sub.paginateAdminDatas",para).paginate(pageNumber, pageSize);
+
+        List<Record> list =new ArrayList<>();
+		if(ObjUtil.isNull(para.getStr("fitemss97subid"))){
+			List<Record> fitemss98list = dbTemplate("fitemss97sub.fitemss98list", para).find();
+			List<Record> fitemssZFlist = dbTemplate("fitemss97sub.fitemssZFlist", para).find();
+			List<Record> fitemss03list = dbTemplate("fitemss97sub.fitemss03list", para).find();
+			List<Record> fitemss00list = dbTemplate("fitemss97sub.fitemss00list", para).find();
+			list=Stream.of(fitemss00list,fitemss98list, fitemss03list,fitemssZFlist ).flatMap(Collection::stream).collect(Collectors.toList());
+		}else{
+			Record first = dbTemplate("fitemss97.findfitemss97List", Kv.by("fitemss97subid", para.getStr("fitemss97subid"))).findFirst();
+			/**
+			 * 判断是否有子项
+			 */
+			String fitemss97subid = selectSub(para.getStr("fitemss97subid"));
+			para.set("fitemss97subid",fitemss97subid);
+				switch (first.getStr("citemccode")){
+					case "1":
+						List<Record> fitemss00list = dbTemplate("fitemss97sub.fitemss00list", para).find();
+						list=Stream.of(fitemss00list).flatMap(Collection::stream).collect(Collectors.toList());
+						break;
+					case "2":
+						List<Record> fitemss98list = dbTemplate("fitemss97sub.fitemss98list", para).find();
+						list=Stream.of(fitemss98list).flatMap(Collection::stream).collect(Collectors.toList());
+						break;
+					case "3":
+						List<Record> fitemss03list = dbTemplate("fitemss97sub.fitemss03list", para).find();
+						list=Stream.of(fitemss03list).flatMap(Collection::stream).collect(Collectors.toList());
+						break;
+					case "4":
+						List<Record> fitemssZFlist = dbTemplate("fitemss97sub.fitemssZFlist", para).find();
+						list=Stream.of(fitemssZFlist).flatMap(Collection::stream).collect(Collectors.toList());
+						break;
+					default:
+						List<Record> fitemss98list1 = dbTemplate("fitemss97sub.fitemss98list", para).find();
+						List<Record> fitemssZFlist1 = dbTemplate("fitemss97sub.fitemssZFlist", para).find();
+						List<Record> fitemss03list1 = dbTemplate("fitemss97sub.fitemss03list", para).find();
+						List<Record> fitemss00list1 = dbTemplate("fitemss97sub.fitemss00list", para).find();
+						list=Stream.of(fitemss00list1,fitemss98list1, fitemss03list1,fitemssZFlist1 ).flatMap(Collection::stream).collect(Collectors.toList());
+				}
+
+
+
+
+		}
+		long totalRow;
+		totalRow=list.size();
+		int totalPage = (int) (totalRow / pageSize);
+		if (totalRow % pageSize != 0) {
+			totalPage++;
+		}
+		List<Record> recordArrayList = new ArrayList<>();
+		int pageStart=pageNumber==1?0:(pageNumber-1)*pageSize;//截取的开始位置
+		int pageEnd= Math.min((int) totalRow, pageNumber * pageSize);
+		if(totalRow>pageStart){
+			recordArrayList =list.subList(pageStart, pageEnd);
+		}
+
+		return  new Page<>(recordArrayList, pageNumber, pageSize, totalPage, (int) totalRow);
+
 	}
+
+
+	//判断是否有有子项，有的话把子项全找出来
+    public String selectSub(String fitemss97subid) {
+		List<Record> list = dbTemplate("fitemss97.findfitemss97sub", Kv.by("isourceid", fitemss97subid)).find();
+		if(list.size()!=0){
+			StringJoiner stringJoiner=new StringJoiner(",");
+			for (Record record: list){
+				List<Record> subList = dbTemplate("fitemss97.findfitemss97sub", Kv.by("isourceid",  record.get("iautoid"))).find();
+				if(subList.size()!=0){
+					for (Record sub :subList) {
+						String iautoid = sub.getStr("iautoid");
+						stringJoiner.add(iautoid);
+					}
+				}else{
+					stringJoiner.add(record.getStr("iautoid"));
+				}
+
+			}
+			return stringJoiner.toString();
+		}
+			return fitemss97subid;
+	}
+
+
+	/**
+	 * 得到后台分类数据树 包含所有数据
+	 *
+	 *
+	 * @param openLevel 打开级别
+	 */
+	public List<JsTreeBean> getMgrTree(int openLevel, String sn) {
+
+		/**
+		 * 项目大类目录
+		 */
+
+
+		List<JsTreeBean> jsTreeBeanList = new ArrayList<>();
+
+			List<Record> subRecords = dbTemplate("fitemss97.findfitemss97List",Kv.by("citemccode",sn)).find();
+			for (Record subRecord : subRecords) {
+				Long id = subRecord.getLong("iAutoId");
+				Object pid = subRecord.getStr("iSourceId");
+				String text = "[" + subRecord.getStr("citemcode") + "]" + subRecord.getStr("citemname");
+				String type = subRecord.getStr("citemcode");
+				JsTreeBean jsTreeBean = new JsTreeBean(id, pid, text, type, "", false);
+				jsTreeBeanList.add(jsTreeBean);
+		}
+
+
+		return jsTreeBeanList;
+	}
+
 
 	/**
 	 * 保存
