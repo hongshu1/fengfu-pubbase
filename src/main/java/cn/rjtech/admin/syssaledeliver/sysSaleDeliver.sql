7a107044fcaae3798212619ad8d381c5c7373d13
洪 <1479962959@qq.com>
2023-07-04 19:35:01 +0800

    销售出库列表提交

--- src/main/java/cn/rjtech/admin/syssaledeliver/sysSaleDeliver.sql
+++ src/main/java/cn/rjtech/admin/syssaledeliver/sysSaleDeliver.sql
@@ -2,16 +2,19 @@
 #sql("pageList")
 SELECT
     t1.*,
-    t2.WhCode,
-    t2.PosCode,
-    t2.CusBarcode,
-    t2.Barcode,
-    t2.InvCode,
-    t2.Num,
-    t2.Qty
+    (SELECT TOP 1 Wa.cWhName FROM T_Sys_SaleDeliverDetail  LEFT JOIN Bd_Warehouse Wa ON WhCode = Wa.cWhCode) AS WhName,
+    Rd.cRdName AS RdName,
+    de.cDepName AS DepName,
+    cus.cCusName AS CusName
 FROM
-    T_Sys_SaleDeliver t1 JOIN T_Sys_SaleDeliverDetail t2 ON t1.AutoID = t2.MasID
+    T_Sys_SaleDeliver t1
+        LEFT JOIN Bd_Rd_Style Rd ON t1.RdCode = Rd.cRdCode
+        LEFT JOIN Bd_Department de ON t1.DeptCode =de.cDepCode
+        LEFT JOIN Bd_Customer cus ON  t1.VenCode=cus.cCusCode
 WHERE 1=1
+    #if(sqlids)
+    AND t1.AutoID in (#(sqlids))
+    #end
 #if(billNo)
     AND t1.BillNo LIKE CONCAT('%', #para(BillNo), '%')
 #end
@@ -74,6 +77,39 @@ where 1=1
 #if(masId)
     and t1.MasID = #para(masId)
 #end
+#end
+
+
+#sql("getFormDatas")
+SELECT
+    t1.*,
+    t2.WhCode,
+    (SELECT cWhName FROM Bd_Warehouse WHERE t2.WhCode = cWhCode) AS WhName,
+    Rd.cRdName AS RdName,
+    de.cDepName AS DepName,
+    cus.cCusName AS CusName,
+    t2.PosCode,
+    t2.CusBarcode,
+    t2.Barcode,
+    t2.InvCode,
+    t2.Num,
+    ( SELECT SUM(Qty)  FROM T_Sys_SaleDeliverDetail WHERE t1.AutoID = t2.MasID ) AS MaxQty
+FROM
+    T_Sys_SaleDeliver t1
+        LEFT JOIN T_Sys_SaleDeliverDetail t2 ON t1.AutoID = t2.MasID
+        LEFT JOIN Bd_Rd_Style Rd ON t1.RdCode = Rd.cRdCode
+        LEFT JOIN Bd_Department de ON t1.DeptCode =de.cDepCode
+        LEFT JOIN Bd_Customer cus ON  t1.VenCode=cus.cCusCode
+WHERE
+    1 = 1
+    #if(AutoID)
+    and t1.AutoID = #para(AutoID)
+    #end
+#if(OrganizeCode)
+    and t1.OrganizeCode = #para(OrganizeCode)
+#end
+ORDER BY
+    t1.createDate DESC
 #end
 
 
