eb59ab66c72e17650464e757e3ceb17bd9cae71b
hzx <ytlk_hzx@163.com>
2023-07-07 00:02:00 +0800

    项目档案-修正——项目大类-同步删除——项目分类-同步删除

--- src/main/java/cn/rjtech/admin/fitemss97sub/fitemss97sub.sql
+++ src/main/java/cn/rjtech/admin/fitemss97sub/fitemss97sub.sql
@@ -1,19 +1,17 @@
 
 #sql("fitemss00list")
-SELECT
-    Bd_fitemss97.iautoid as iautoid,
-    Bd_fitemss00.citemcode as citemcode,
-    Bd_fitemss00.citemname as citemname,
-    Bd_fitemss00.bclose as  bclose,
-    Bd_fitemss97.citemcode as citemccode ,
-    Bd_fitemss97.citemname as citemcname
-FROM Bd_fitemss97
-         JOIN Bd_fitemss00class ON Bd_fitemss97.citemcode=Bd_fitemss00class.cItemCcode
-         JOIN Bd_fitemss00 ON  Bd_fitemss00class.cItemCcode=Bd_fitemss00.citemccode
-WHERE Bd_fitemss97.citemccode='00' and Bd_fitemss97.isDeleted = '0' and Bd_fitemss00class.isDeleted = '0'
+SELECT     Bd_fitemss00class.iautoid as iautoid,
+           Bd_fitemss00.citemcode as citemcode,
+           Bd_fitemss00.citemname as citemname,
+           Bd_fitemss00.bclose as  bclose,
+           Bd_fitemss00class.citemccode as citemccode ,
+           Bd_fitemss00class.citemcname as citemcname
+FROM	Bd_fitemss00class
+            JOIN Bd_fitemss00 ON    Bd_fitemss00class.cItemCcode=Bd_fitemss00.citemccode
+WHERE   Bd_fitemss00class.isDeleted = '0'
   and Bd_fitemss00.isDeleted = '0'
     #if(fitemss97subid)
-			and Bd_fitemss97.iAutoId IN (
+			and Bd_fitemss00class.iautoid IN (
             #for(v:fitemss97subid.split(','))
             '#(v)' #(for.last?'':',')
             #end
@@ -28,21 +26,19 @@ Order By citemcode asc
 
 
     #sql("fitemss98list")
-SELECT
-    Bd_fitemss97.iautoid as iautoid,
-    Bd_fitemss98.citemcode as citemcode,
-    Bd_fitemss98.citemname as citemname,
-    Bd_fitemss98.bclose as  bclose,
-    Bd_fitemss98.cDirection as cdirection,
-    Bd_fitemss97.citemcode as citemccode ,
-    Bd_fitemss97.citemname as citemcname
-FROM Bd_fitemss97
-         JOIN Bd_fitemss98class ON Bd_fitemss97.citemcode=Bd_fitemss98class.cItemCcode
-         JOIN Bd_fitemss98 ON  Bd_fitemss98class.cItemCcode=Bd_fitemss98.citemccode
-WHERE Bd_fitemss97.citemccode='98' and Bd_fitemss97.isDeleted = '0' and Bd_fitemss98class.isDeleted = '0'
+SELECT     Bd_fitemss98class.iautoid as iautoid,
+           Bd_fitemss98.citemcode as citemcode,
+           Bd_fitemss98.citemname as citemname,
+           Bd_fitemss98.bclose as  bclose,
+           Bd_fitemss98.cDirection as cdirection,
+           Bd_fitemss98class.citemccode as citemccode ,
+           Bd_fitemss98class.citemcname as citemcname
+FROM	Bd_fitemss98class
+  JOIN Bd_fitemss98 ON    Bd_fitemss98class.cItemCcode=Bd_fitemss98.citemccode
+WHERE   Bd_fitemss98class.isDeleted = '0'
   and Bd_fitemss98.isDeleted = '0'
     #if(fitemss97subid)
-			and Bd_fitemss97.iAutoId IN (
+			and  Bd_fitemss98class.iautoid IN (
             #for(v:fitemss97subid.split(','))
             '#(v)' #(for.last?'':',')
             #end
@@ -56,20 +52,18 @@ WHERE Bd_fitemss97.citemccode='98' and Bd_fitemss97.isDeleted = '0' and Bd_fitem
 
 
  #sql("fitemss03list")
-SELECT
-    Bd_fitemss97.iautoid as iautoid,
-    Bd_fitemss03.citemcode as citemcode,
-    Bd_fitemss03.citemname as citemname,
-    Bd_fitemss03.bclose as  bclose,
-    Bd_fitemss97.citemcode as citemccode ,
-    Bd_fitemss97.citemname as citemcname
-FROM Bd_fitemss97
-         JOIN Bd_fitemss03class ON Bd_fitemss97.citemcode=Bd_fitemss03class.cItemCcode
-         JOIN Bd_fitemss03 ON  Bd_fitemss03class.cItemCcode=Bd_fitemss03.citemccode
-WHERE Bd_fitemss97.citemccode='03' and Bd_fitemss97.isDeleted = '0' and Bd_fitemss03class.isDeleted = '0'
+SELECT     Bd_fitemss03class.iautoid as iautoid,
+           Bd_fitemss03.citemcode as citemcode,
+           Bd_fitemss03.citemname as citemname,
+           Bd_fitemss03.bclose as  bclose,
+           Bd_fitemss03class.citemccode as citemccode ,
+           Bd_fitemss03class.citemcname as citemcname
+FROM	Bd_fitemss03class
+            JOIN Bd_fitemss03 ON    Bd_fitemss03class.cItemCcode=Bd_fitemss03.citemccode
+WHERE   Bd_fitemss03class.isDeleted = '0'
   and Bd_fitemss03.isDeleted = '0'
     #if(fitemss97subid)
-			and Bd_fitemss97.iAutoId IN (
+			and Bd_fitemss03class.iautoid IN (
             #for(v:fitemss97subid.split(','))
             '#(v)' #(for.last?'':',')
             #end
@@ -82,21 +76,19 @@ Order By citemcode asc
     #end
 
 #sql("fitemssZFlist")
-SELECT
-    Bd_fitemss97.iautoid as iautoid,
-    Bd_fitemssZF.citemcode as citemcode,
-    Bd_fitemssZF.citemname as citemname,
-    Bd_fitemssZF.bclose as  bclose,
-    Bd_fitemssZF.cDirection as cdirection,
-    Bd_fitemss97.citemcode as citemccode ,
-    Bd_fitemss97.citemname as citemcname
-FROM Bd_fitemss97
-         JOIN Bd_fitemssZFclass ON Bd_fitemss97.citemcode=Bd_fitemssZFclass.cItemCcode
-         JOIN Bd_fitemssZF ON  Bd_fitemssZFclass.cItemCcode=Bd_fitemssZF.citemccode
-WHERE Bd_fitemss97.citemccode='ZF' and Bd_fitemss97.isDeleted = '0' and Bd_fitemssZFclass.isDeleted = '0'
+SELECT     Bd_fitemssZFclass.iautoid as iautoid,
+           Bd_fitemssZF.citemcode as citemcode,
+           Bd_fitemssZF.citemname as citemname,
+           Bd_fitemssZF.bclose as  bclose,
+           Bd_fitemssZF.cDirection as cdirection,
+           Bd_fitemssZFclass.citemccode as citemccode ,
+           Bd_fitemssZFclass.citemcname as citemcname
+FROM	Bd_fitemssZFclass
+            JOIN Bd_fitemssZF ON    Bd_fitemssZFclass.cItemCcode=Bd_fitemssZF.citemccode
+WHERE   Bd_fitemssZFclass.isDeleted = '0'
   and Bd_fitemssZF.isDeleted = '0'
     #if(fitemss97subid)
-			and Bd_fitemss97.iAutoId IN (
+			and Bd_fitemssZFclass.iautoid IN (
             #for(v:fitemss97subid.split(','))
             '#(v)' #(for.last?'':',')
             #end
