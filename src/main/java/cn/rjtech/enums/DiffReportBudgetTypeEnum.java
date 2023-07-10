efb0659c8d4f4de029130730ae1625eed0244692
Kephon <kaifanlai@qq.com>
2023-07-10 20:34:14 +0800

    【调整】code review

--- src/main/java/cn/rjtech/enums/DiffReportBudgetTypeEnum.java
+++ src/main/java/cn/rjtech/enums/DiffReportBudgetTypeEnum.java
@@ -11,35 +11,35 @@ public enum DiffReportBudgetTypeEnum {
     /**
      * 投资计划预算类型枚举列表
      */
-    FUALL_YEAR_BUDGET(1, "全年预算a"),
-    NEXT_PERIOD_EDIT(2, "下半年修订b"),
-	ACTUALBUDGET(3, "实绩预测d"),
-	ACTUAL(4, "实绩e"),
-	DIFF_A(5, "差异A（b-a）"),
-	DIFF_E(6, "差异E（d-a）"),
-	DIFF_F(7, "差异F（d-b）"),
-	DIFF_G(8, "差异G（e-a）"),
-	DIFF_H(9, "差异H（e-b）"),
-	DIFF_J(10, "差异J（e-d）");
+    FUALL_YEAR_BUDGET("全年预算a", 1),
+    NEXT_PERIOD_EDIT("下半年修订b", 2),
+    ACTUALBUDGET("实绩预测d", 3),
+    ACTUAL("实绩e", 4),
+    DIFF_A("差异A（b-a）", 5),
+    DIFF_E("差异E（d-a）", 6),
+    DIFF_F("差异F（d-b）", 7),
+    DIFF_G("差异G（e-a）", 8),
+    DIFF_H("差异H（e-b）", 9),
+    DIFF_J("差异J（e-d）", 10);
 
-    private static final Map<Integer, DiffReportBudgetTypeEnum> DiffReportBudgetTypeEnum_MAP = new HashMap<>();
+    private static final Map<Integer, DiffReportBudgetTypeEnum> REPORT_BUDGET_TYPE_ENUM_MAP = new HashMap<>();
 
     static {
         for (DiffReportBudgetTypeEnum statusEnum : DiffReportBudgetTypeEnum.values()) {
-        	DiffReportBudgetTypeEnum_MAP.put(statusEnum.value, statusEnum);
+            REPORT_BUDGET_TYPE_ENUM_MAP.put(statusEnum.value, statusEnum);
         }
     }
 
     private final String text;
     private final int value;
 
-    DiffReportBudgetTypeEnum(int value, String text) {
-        this.value = value;
+    DiffReportBudgetTypeEnum(String text, int value) {
         this.text = text;
+        this.value = value;
     }
 
-    public static DiffReportBudgetTypeEnum toEnum(int status) {
-        return DiffReportBudgetTypeEnum_MAP.get(status);
+    public static DiffReportBudgetTypeEnum toEnum(int value) {
+        return REPORT_BUDGET_TYPE_ENUM_MAP.get(value);
     }
 
     public int getValue() {
@@ -53,8 +53,8 @@ public enum DiffReportBudgetTypeEnum {
     @Override
     public String toString() {
         return "DiffReportBudgetTypeEnum{" +
-                "value=" + value +
-                ", text=" + text +
+                "text='" + text + '\'' +
+                ", value=" + value +
                 '}';
     }
 
