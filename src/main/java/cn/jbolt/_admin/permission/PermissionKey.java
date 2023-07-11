e622472a74a92622287dda6a8be29f107e33eb10
洪 <1479962959@qq.com>
2023-07-11 17:31:53 +0800

    特殊领料，调拨单按钮权限，自测

--- src/main/java/cn/jbolt/_admin/permission/PermissionKey.java
+++ src/main/java/cn/jbolt/_admin/permission/PermissionKey.java
@@ -3961,48 +3961,4 @@ public class PermissionKey {
 	 * 删除
 	 */
 	public static final String OTHER_OUT_RETURN_LIST_DELETE = "other_out_return_list_delete";
-	/**
-	 * 提交
-	 */
-	public static final String WORKCALENDARM_SUBMIT = "workcalendarm_submit";
-	/**
-	 * 新增
-	 */
-	public static final String MATERIAL_RETURN_LIST_ADD = "material_return_list_add";
-	/**
-	 * 编辑
-	 */
-	public static final String MATERIAL_RETURN_LIST_EDIT = "material_return_list_edit";
-	/**
-	 * 删除
-	 */
-	public static final String MATERIAL_RETURN_LIST_DELETE = "material_return_list_delete";
-	/**
-	 * 导出
-	 */
-	public static final String PLANYEARSUM_EXPORT = "planyearsum_export";
-	/**
-	 * 新增
-	 */
-	public static final String TRANSVOUCH_ADD = "transvouch_add";
-	/**
-	 * 编辑
-	 */
-	public static final String TRANSVOUCH_EDIT = "transvouch_edit";
-	/**
-	 * 删除
-	 */
-	public static final String TRANSVOUCH_DELETE = "transvouch_delete";
-	/**
-	 * 查看
-	 */
-	public static final String PLANYEARSUM_SHOW = "planyearsum_show";
-	/**
-	 * 导出
-	 */
-	public static final String PLANMONTHSUM_EXPORT = "planmonthsum_export";
-	/**
-	 * 查看
-	 */
-	public static final String PLANMONTHSUM_SHOW = "planmonthsum_show";
 }
