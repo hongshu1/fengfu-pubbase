b04e4e3e25ded00c3c405dbfaa40003d55c8dac0
timi <246151394592qq.com>
2023-07-11 13:16:20 +0800

    【调整】  1.编码规则bug调整  2.添加编码规则权限操作按钮控制

--- src/main/java/cn/rjtech/admin/codingrulem/codingrulem.sql
+++ src/main/java/cn/rjtech/admin/codingrulem/codingrulem.sql
@@ -1,9 +1,31 @@
 #sql("findByTable")
 SELECT TOP 1 m.*
-FROM Bd_CodingRuleM m 
+FROM Bd_CodingRuleM m
 INNER JOIN Bd_Form f ON m.iformid = f.iautoid
 WHERE f.cFormCode = #para(cformcode)
     AND f.iorgid = #para(iorgid)
     AND f.isDeleted = '0'
     AND m.isDeleted = '0'
+#end
+
+#sql("datas")
+SELECT
+	f.cFormCode,
+	f.cFormName,
+	m.*
+FROM
+	Bd_CodingRuleM m
+	LEFT JOIN Bd_Form f ON f.iAutoId = m.iFormId
+WHERE
+	m.IsDeleted = '0'
+	#if(iCodingType)
+	    AND m.iCodingType = #para(iCodingType)
+	#end
+	#if(cFormTypeSn)
+	    AND m.cFormTypeSn = #para(cFormTypeSn)
+	#end
+	#if(cFormName)
+	    AND f.cFormName LIKE concat('%',#para(cFormName),'%')
+	#end
+	ORDER BY m.dCreateTime DESC
 #end
